88e07012740e6da61810866de184ddbb4ccd6333
marty <marty@martypdx.com>
2016-03-01 11:46:24 -0800

    stray mods

--- week8/feb_25_user_auth/app/src/services/index.js
+++ week8/feb_25_user_auth/app/src/services/index.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import resources from './resources';
 import user from './user';
+import dialog from './dialog';
 
 const services = angular.module( 'services', [] );
 
@@ -11,6 +12,7 @@ services.config( [ '$provide', function( $provide ){
 	$provide.constant( 'baseUrl', baseUrl );
 }]);
 
+dialog( services );
 resources( services );
 user( services );
 
